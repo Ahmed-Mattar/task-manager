fa0017a6a4643d434354782e1df502e6227af1ee
Ahmed Mattar <AceMattar@gmail.com>
2019-07-21 20:17:24 +0200

    feat:inserting multiple documents

--- mongodb.js
+++ mongodb.js
@@ -12,9 +12,49 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) =>{
 
     const db  = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: 'Ahmed',
-        age: 24
+    // db.collection('users').insertOne({
+    //     name: 'Ahmed',
+    //     age: 24
+    // }, (error, result) => {
+    //     if(error){
+    //         return console.log('Unable to insert user')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('users').insertMany([{
+    //       name: 'Ramy',
+    //       age: 34  
+    // },{
+    //       name:'riham',
+    //       age:29
+    // }],(error,result)=>{
+    //     if(error){
+    //         return console.log('Unable to insert documents!')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+        description : "walk the dog",
+        completed: false
+        },
+        {
+        description : "walk the cat",
+        completed: false
+        },
+        {
+        description : "buy a new tv",
+        completed: true
+        }],(error,result) => {
+            if(error){
+              return console.log('Unable to insert documents!')
+            }
+
+            console.log(result.ops)
     })
     
 })
