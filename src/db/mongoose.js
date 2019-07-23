e362a4a7caca4e3192451f159bcd0fa2735aab59
Ahmed Mattar <AceMattar@gmail.com>
2019-07-23 02:45:11 +0200

    feat: creating a mongoose model

---
+++ src/db/mongoose.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+    useNewUrlParser: true,
+    useCreateIndex: true
+})
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required:true
+    },
+    age: {
+        type: Number,
+        validate(value) {
+            if(value < 0){
+                throw new Error('Age must be a positive number')
+            }
+        }
+    }
+})
+
+const me = new User({
+    name: 'Mattar',
+})
+
+me.save().then(()=>{
+    console.log(me)
+}).catch((error)=>{
+    console.log('Error!', error)
+})
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String
+    },
+    completed:{
+        type: Boolean
+    }
+})
+
+// const task = new Task({
+//     description:"walk the dog",
+//     completed:true
+// })
+
+// task.save().then(()=>{
+//     console.log(task)
+// }).catch((error)=>{
+//     console.log(error)
+// })
