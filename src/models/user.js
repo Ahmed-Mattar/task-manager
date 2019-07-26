ba106ebd887c10867a8cf50feb4c72ee193f25b5
Ahmed Mattar <AceMattar@gmail.com>
2019-07-26 04:28:57 +0200

    feat: implement basic login

--- src/models/user.js
+++ src/models/user.js
@@ -21,6 +21,7 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
+        unique: true,
         required: true,
         trim: true,
         lowercase: true,
@@ -41,6 +42,24 @@ const userSchema = new mongoose.Schema({
     }
 })
 
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+
+    if(!user){
+        throw new Error('Unable to login')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if(!isMatch) {
+        throw new Error('Unable to login')
+    }
+
+    return user
+}
+
+
+// Hash the plain text password before saving
 userSchema.pre('save', async function (next) {
     // this gives us access to the individual document that are going to be saved
     const user = this
