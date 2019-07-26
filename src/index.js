5d1df1c0eeb66d331310a597cc1fa9580acb641e
Ahmed Mattar <AceMattar@gmail.com>
2019-07-27 01:57:45 +0200

    feat: using express middleware

--- src/index.js
+++ src/index.js
@@ -7,12 +7,16 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
 app.use((req, res, next) => {
-    if (req.method === 'GET') {
-        res.send('GET requests are disabled')
-    } else {
-        next()
-    }
+     res.status(503).send("Site is currently down. check back soon")
 })
 
 app.use(express.json())
