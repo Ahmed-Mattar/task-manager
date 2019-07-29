55a649b4af93140c12bd25774e045d3227e899aa
Ahmed Mattar <AceMattar@gmail.com>
2019-07-29 03:26:17 +0200

    refactor: seperate index.js into 2 files

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+require('./db/mongoose')
+
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+
+const app = express()
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+//
+// without middleware: new request -> run route handler
+//
+// with middleware: new request -> do something -> run route handler
+//
+
+module.exports = app
